eb710885c88b4f150d00fc97f71d0218d74d221a
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2021-11-15 17:22:06 +0100

    updated set up file

--- bpa-set-up.py
+++ bpa-set-up.py
@@ -1,6 +1,7 @@
 import os
 
-os.system("sudo apt-get install bluelog blueranger bluesnarfer")
-## TODO: missing sdptool btlesniffer
+os.system("sudo apt-get install bluelog blueranger bluesnarfer bluez")
+os.system("sudo apt-get install bluetooth libbluetooth-dev")
 os.system("sudo pip install pybluez pwntools")
+os.system("pip install -U git+https://github.com/scipag/btle-sniffer.git")
 
